2180377b17eb8324c9fbe7c5a1aa4e2cfa42ca84
VloneMe <francisntima@yahoo.com>
2023-03-25 14:30:33 -0700

    the precision for printing

---
+++ 4-function.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * get_precision - This Calculates the precision for printing
+ *
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Prec.
+ */
+
+int get_precision(const char *format, int *i, va_list list)
+{
+
+	int curr_i = *i + 1;
+	int prec = -1;
+
+	if (format[curr_i] != '.')
+		return (prec);
+
+	prec = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			prec *= 10;
+			prec += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			prec = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (prec);
+
+}

ce35ffe39f2cf7e9c97cd5ba5e0d862a06ce4ab0
VloneMe <francisntima@yahoo.com>
2023-03-25 14:05:47 -0700

    conversion specifiers for u, o, x, X

---
+++ 3-function.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_flags - This function Calculates active flags
+ *
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ *
+ * Return: Flags:
+ *
+ */
+
+int get_flags(const char *format, int *i)
+{
+
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+
+	int n, curr_i;
+	int flags = 0;
+
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (n = 0; FLAGS_CH[n] != '\0'; n++)
+			if (format[curr_i] == FLAGS_CH[n])
+			{
+				flags |= FLAGS_ARR[n];
+				break;
+			}
+
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (flags);
+
+}
